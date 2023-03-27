e328d49b63fd0f3fdca0459a3abf7a4330d0cb23
Vishal0318 <128881589+Vishal0318@users.noreply.github.com>
2023-03-27 10:55:19 +0530

    Add files via upload

---
+++ Example1 (1).java
@@ -0,0 +1,18 @@
+package day3_switch_loops;
+
+import java.util.Scanner;
+
+public class Example1 {
+
+	public static void main(String[] args) 
+	{
+ 
+		 
+
+		byte b= 10;
+		b= (byte) (b+1);
+		System.out.println(b);
+		 
+	}
+
+}
